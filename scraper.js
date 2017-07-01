de476431c6e7ca44a41538342597ca903ca26960
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-07-01 14:40:39 +0900

    fix data bug

--- scraper.js
+++ scraper.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const list = 'http://asamaonsen.jp/asamap/%E5%A4%96%E6%B9%AF%E3%81%A8%E7%A5%AD%E3%82%8A%E6%A1%88%E5%86%85/';
 
 client.fetch(list).then( result => {
+	console.log('--- Fetch URL List ---')
 	let $ = result.$
 	return $('.asamap-list li a').map( (i,obj) => {
 		if( $(obj).text().indexOf('【湯】') != -1 ) {
@@ -12,30 +13,40 @@ client.fetch(list).then( result => {
 	}).get();
 
 }).then( links => {
-	return links.map( (link) => {
+	console.log('--- Fetching onsen data ---')
+	return Promise.all( links.map( (link) => {
 		return client.fetch(link)
-	})
-}).then((p) => {
-	return Promise.all(p)
-
+	}))
 }).then(results => {
-	"use strict";
+	console.log('--- Fetched onsen data ---')
 	let data = results.map( result => {
 		let $ = result.$
-		let title = $('#main_image3 h2').text().replace( /【湯】/g , "" )
-		let tds = $('.culture td');
-		let address = tds.eq(0).text().replace(/〒390-0303　|\[地図\]/g,'');
-		let tel = tds.eq(1).text();
-		let price = tds.eq(2).text();
+		let data = {
+			title: '',
+			address: '',
+			tel: '',
+			price: ''
+		};
+		data.title = $('#main_image3 h2').text().replace( /【湯】/g , "" );
+		let rows = $('.culture tr');
+		rows.each((i,row) => {
+			"use strict";
+			switch ($(row).find('th').text()) {
+				case '住所':
+					data.address = $(row).find('td').text();
+					break;
+				case 'TEL':
+					data.tel = $(row).find('td').text();
+					break;
+				case '料金':
+					data.price = $(row).find('td').text();
+					break;
+			}
 
-		return {
-			title,
-			address,
-			tel,
-			price
-		}
+		});
+		return data
 	})
-
+	console.log('--- Create JSON ---')
 	fs.writeFileSync('data.json', JSON.stringify(data, null, "    "));
 
 })
